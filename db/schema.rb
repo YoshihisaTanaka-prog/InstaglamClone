08c6b8bfc25395acf3ad71ad1a2ce6df0cb6ff3e
shiho imamura <zhibao29@yahoo.co.jp>
2023-01-18 15:53:19 +0900

    ログインシステム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_140523) do
+ActiveRecord::Schema.define(version: 2023_01_18_062312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2023_01_17_140523) do
     t.string "title"
     t.text "content"
     t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
